3a1e66560dc7c14dfc516629501372d66cba8405
LAPTOP-DLALDVTD\arinc <arinchintya31@gmail.com>
2021-03-20 11:53:05 +0700

    Pertemuan 4

---
+++ Pert4_Latihan10_Perkalian.php
@@ -0,0 +1,22 @@
+<html> 
+    <head> 
+        <title>Tabel Perkalian</title> 
+    </head> 
+    <body> 
+
+
+<?php
+    echo "=============== <br>";
+    echo "Tabel Perkalian <br>";
+    echo "=============== <br>";
+
+$a = 12;
+
+    for ($i =15; $i < 46; $i+=2) {
+
+        $hasil = $a*$i;
+        echo "$a * $i = $hasil <br>";
+}
+?>
+</body> 
+</html> 
